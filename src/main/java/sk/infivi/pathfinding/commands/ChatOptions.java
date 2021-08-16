599b65b3f575ef000d1fae3ec8ea0a15618afaef
0xVector <vector4.vector4@gmail.com>
2021-08-16 22:46:50 +0200

    Added chat menu block plane check

--- src/main/java/sk/infivi/pathfinding/commands/ChatOptions.java
+++ src/main/java/sk/infivi/pathfinding/commands/ChatOptions.java
@@ -11,6 +11,7 @@ import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import sk.infivi.pathfinding.Manager;
 import sk.infivi.pathfinding.algorithms.PathfindingAlgorithmType;
+import sk.infivi.pathfinding.graph.Graph;
 import sk.infivi.pathfinding.visualization.BlockPlane;
 import sk.infivi.pathfinding.visualization.DrawMode;
 
@@ -24,13 +25,17 @@ public class ChatOptions implements CommandExecutor  {
     // Constant components
     static final Component choiceHoverComponent = text("Click to select", GREEN, BOLD);
     static final Component space = text("   ");
+    static final Component tick = text("✔", GREEN, BOLD);
+    static final Component cross = text("✘", RED, BOLD);
 
     // Styles
     final static Style titleStyle = Style.style(GOLD, BOLD);
     final static Style headerFooterStyle = Style.style(AQUA);
     static final Style optionNameStyle = Style.style(BLUE, BOLD);
     static final Style optionChoiceStyle = Style.style(GREEN, BOLD);
-    static final  Style notSetStyle = Style.style(RED, ITALIC);
+    static final Style notSetStyle = Style.style(RED, ITALIC);
+    static final Style trueStyle = Style.style(GREEN);
+    static final Style falseStyle = Style.style(RED);
 
     private final Manager manager;
 
@@ -45,86 +50,118 @@ public class ChatOptions implements CommandExecutor  {
 
         optionsMessage.append(
 
-            // Title
-            newline(),
-            text("========[ ", headerFooterStyle),
-            text("Options", titleStyle),
-            text(" ]========", headerFooterStyle),
-            newline(),
+                // Title
+                newline(),
+                text("========[ ", headerFooterStyle),
+                text("Options", titleStyle),
+                text(" ]========", headerFooterStyle),
+                newline(),
 
-            // Ready state
-            text("Ready: ", optionNameStyle), space,
-            text(manager.getReady(), LIGHT_PURPLE, ITALIC),
-            newline(), newline(),
-
-
-            // Algorithm selection
-            text("Algorithm: ", optionNameStyle), space,
-            getChoices(PathfindingAlgorithmType.values()),
-            newline(),
+                // Ready state
+                text("Ready: ", optionNameStyle), space,
+                text(manager.getReady(), LIGHT_PURPLE, ITALIC),
+                newline(), newline(),
 
 
-            // Drawing mode selection
-            text("Drawing mode: ", optionNameStyle), space,
-            getChoices(DrawMode.values()),
-            newline());
+                // Algorithm selection
+                text("Algorithm: ", optionNameStyle), space,
+                getChoices(PathfindingAlgorithmType.values()),
+                newline(),
 
 
-        // Block plane selection TODO: Add information if valid (start + end set)
+                // Drawing mode selection
+                text("Drawing mode: ", optionNameStyle), space,
+                getChoices(DrawMode.values()),
+                newline());
+
+
+        // Block plane selection
         final BlockPlane blockPlane = manager.getBlockPlane();
-        final TextComponent.Builder blockPlaneComponent = empty().toBuilder();
-
-        blockPlaneComponent.append(text("Block plane: ", optionNameStyle), space);
+        final TextComponent.Builder blockPlaneComponent = empty().toBuilder()
+                .append(text("Block plane: ", optionNameStyle), space);
 
         if (blockPlane != null) {
             blockPlaneComponent.append(
-                text(String.format("[%d, %d] to [%d, %d]",
-                blockPlane.xCoord1, blockPlane.zCoord1, blockPlane.xCoord2, blockPlane.zCoord2),
-                optionChoiceStyle));
+                    text("Set ", optionChoiceStyle),
+                    text(String.format("([%d, %d] to [%d, %d])",
+                            blockPlane.xCoord1, blockPlane.zCoord1, blockPlane.xCoord2, blockPlane.zCoord2), GREEN))
+            .hoverEvent(HoverEvent.showText(text("Click to change.", GREEN)));
+
+            // Block plane validity check
+            Component checkComponent;
+            TextComponent.Builder hoverComponent = empty().toBuilder();
+            Graph graph = manager.getBlockPlane().getGraph(); // TODO: Performance??
+
+            if (graph.startNode != null && graph.endNode != null) {
+                checkComponent = tick;
+            } else {
+                checkComponent = cross;
+            }
+
+            if (graph.startNode != null) {
+                hoverComponent.append(
+                        text("Start ", trueStyle), tick, space);
+
+            } else {
+                hoverComponent.append(
+                        text("Start ", falseStyle), cross, space);
+            }
+
+            if (graph.endNode != null) {
+                hoverComponent.append(
+                        text("End ", trueStyle), tick);
+
+            } else {
+                hoverComponent.append(
+                        text("End ", falseStyle), cross);
+            }
+
+            blockPlaneComponent.append(space, checkComponent.hoverEvent(HoverEvent.showText(hoverComponent)));
 
         } else {
-            blockPlaneComponent.append(text("not set", notSetStyle));
+            blockPlaneComponent
+                    .append(text("Not set", notSetStyle))
+            .hoverEvent(HoverEvent.showText(text("Click to select the block plane.", GREEN)));
         }
 
         blockPlaneComponent
-                .clickEvent(ClickEvent.suggestCommand("/selectblockplane "))
-                .hoverEvent(HoverEvent.showText(text("Click to select the block plane.").color(GREEN)));
+                .clickEvent(ClickEvent.suggestCommand("/selectblockplane "));
 
 
         optionsMessage.append(
 
-            blockPlaneComponent, newline(),
+                blockPlaneComponent, newline(),
 
-            // Drawing speed selection
-            text("Speed: ", optionNameStyle), space,
-            text(manager.getSpeed(), optionChoiceStyle)
-                    .clickEvent(ClickEvent.suggestCommand("/speed "))
-                    .hoverEvent(HoverEvent.showText(
-                            text("Click to set the drawing speed.", GREEN))),
-            newline(), newline(),
+                // Drawing speed selection
+                text("Speed: ", optionNameStyle), space,
+                text(manager.getSpeed(), optionChoiceStyle)
+                        .clickEvent(ClickEvent.suggestCommand("/speed "))
+                        .hoverEvent(HoverEvent.showText(
+                                text("Click to set the drawing speed.", GREEN))),
+                newline(), newline(),
 
 
-            // Start button
-            text("Start", DARK_GREEN, BOLD)
-                    .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/startsearch"))
-                    .hoverEvent(HoverEvent.showText(
-                            text("Click to start the search.", GREEN))), space,
+                // Start button
+                text("Start", DARK_GREEN, BOLD)
+                        .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/startsearch"))
+                        .hoverEvent(HoverEvent.showText(
+                                text("Click to start the search.", GREEN))), space,
 
-            // Items button
-            text("Items", GOLD, BOLD)
-                    .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/getitems"))
-                    .hoverEvent(HoverEvent.showText(text("Click to get the items for building.", YELLOW))), space,
+                // Items button
+                text("Items", GOLD, BOLD)
+                        .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/getitems"))
+                        .hoverEvent(HoverEvent.showText(text("Click to get the items for building.", YELLOW))), space,
 
-            // Clear button
-            text("Clear", DARK_RED, BOLD)
-                    .clickEvent(ClickEvent.runCommand("/clearplane"))
-                    .hoverEvent(HoverEvent.showText(text("Click to clear the block plane", RED)
-                                    .append(newline())
-                                    .append(text("Doesn't destroy walls or alter your terrain in any way.", RED)))),
+                // Clear button
+                text("Clear", DARK_RED, BOLD)
+                        .clickEvent(ClickEvent.runCommand("/clearplane"))
+                        .hoverEvent(HoverEvent.showText(text("Click to clear the block plane", RED)
+                                .append(newline())
+                                .append(text("Doesn't destroy walls or alter your terrain in any way.", RED)))),
 
-            // Footer
-            newline(),
-            text("===========================", headerFooterStyle));
+                // Footer
+                newline(),
+                text("===========================", headerFooterStyle));
 
         sender.sendMessage(optionsMessage);
         return true;
